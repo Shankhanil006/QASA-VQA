b1e92588d7732c63d7cf0936cd18f4894053403c
Shankhanil Mitra <35575223+Shankhanil006@users.noreply.github.com>
2025-07-06 19:33:25 +0530

    Add files via upload

--- fastvqa/datasets/__init__.py
+++ fastvqa/datasets/__init__.py
@@ -1,1 +1,35 @@
+## Version 0.0 Dataset API, includes FAST-VQA and its variants
+from .basic_datasets import (
+    FastVQAPlusPlusDataset,
+    FragmentVideoDataset,
+    FragmentImageDataset,
+    ResizedVideoDataset,
+    ResizedImageDataset,
+    CroppedVideoDataset,
+    CroppedImageDataset,
+    get_spatial_fragments,
+    SampleFrames,
+    FragmentSampleFrames,
+    UnifiedFrameSampler,
+)
 
+## Version 1.0 Dataset API, includes DiViDe VQA and its variants
+from .fusion_datasets import SimpleDataset, FusionDataset,  LSVQPatchDataset, FusionDatasetK400
+
+
+__all__ = [
+    "FragmentVideoDataset",
+    "FragmentImageDataset",
+    "ResizedVideoDataset",
+    "ResizedImageDataset",
+    "CroppedVideoDataset",
+    "CroppedImageDataset",
+    "LSVQPatchDataset",
+    "get_spatial_fragments",
+    "SampleFrames",
+    "FragmentSampleFrames",
+    "UnifiedFrameSampler",
+    "SimpleDataset",
+    "FusionDatasetK400",
+    "FusionDataset",
+]
